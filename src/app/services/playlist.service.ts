23ad99ffd9e7801b85b950bd8d4ba9fb3fe2ae45
marcushaddon <haddon.marcus@gmail.com>
2018-01-06 13:53:00 -0600

    Add PlaylistService.getPlaylist/s methods.

--- src/app/services/playlist.service.ts
+++ src/app/services/playlist.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Rx';
+import { CrateService } from '../meta-classes/crate-service';
+import {
+  Playlist,
+  PaginationOptions
+} from '../models';
 
 @Injectable()
-export class PlaylistService {
+export class PlaylistService extends CrateService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    super();
+  }
+
+  getPlaylists(pagination: PaginationOptions = PaginationOptions.Default): Observable<Playlist[]>{
+    const endpoint = this._apiUrl + '/playlist';
+    const options = this.buildOptions(pagination);
+    return this.http.get<Playlist[]>(endpoint, options);
+  }
+
+  getPlaylist(playlistId: string): Observable<Playlist> {
+    const endpoint = this._apiUrl + `/playlist/${playlistId}`;
+    return this.http.get<Playlist>(endpoint);
+  }
 
 }
