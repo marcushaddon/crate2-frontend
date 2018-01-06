58a300a15a5085a4228e404b7db3ac8ab07ad63f
marcushaddon <haddon.marcus@gmail.com>
2018-01-06 13:02:23 -0600

    Add Post/Put/Delete methods to AlbumService

--- src/app/services/album.service.ts
+++ src/app/services/album.service.ts
@@ -16,21 +16,36 @@ export class AlbumService extends CrateService {
     super();
   }
 
-  getAlbums(pagination: PaginationOptions = PaginationOptions.Default) {
+  getAlbums(pagination: PaginationOptions = PaginationOptions.Default): Observable<Album[]> {
     const endpoint = this._apiUrl + 'album';
     const options = this.buildOptions(pagination);
     return this.http.get<Album[]>(endpoint, options);
   }
 
-  getAlbum(albumId: string, pagination = PaginationOptions.Default) {
+  getAlbum(albumId: string, pagination = PaginationOptions.Default): Observable<Album> {
     const endpoint = this._apiUrl + `album/${albumId}`;
     const options = this.buildOptions(pagination);
     return this.http.get<Album>(endpoint, options);
   }
 
-  getAlbumTracks(albumId: string) {
+  getAlbumTracks(albumId: string): Observable<Track[]> {
     const endpoint = this._apiUrl + `album/${albumId}/tracks`;
     return this.http.get<Track[]>(endpoint);
+  }
+
+  createAlbum(album: Album): Observable<Album> {
+    const endpoint = this._apiUrl + 'album';
+    return this.http.post<Album>(endpoint, album);
+  }
+
+  updateAlbum(album: Album): Observable<Album> {
+    const endpoint = this._apiUrl + `album/${album._id}`;
+    return this.http.put<Album>(endpoint, album);
+  }
+
+  deleteAlbum(album: Album): Observable<Response> {
+    const endpoint = this._apiUrl + `album/${album._id}`;
+    return this.http.delete<Response>(endpoint);
   }
 
 }
